834cdb1f5506725ec081095afd5a22d26befd9f6
Thomas Breloff <tom@breloff.com>
2016-05-26 11:19:05 -0400

    changed linetypes example

--- src/example_generation.jl
+++ src/example_generation.jl
@@ -82,11 +82,12 @@ const _examples = PlotExample[
   PlotExample("Line types",
               "",
               [
-                :(types = intersect(supportedTypes(), [:line, :path, :steppre, :steppost, :sticks, :scatter])'),
-                :(n = length(types)),
+                # :(types = intersect(supportedTypes(), [:line, :path, :steppre, :steppost, :sticks, :scatter])'),
+                :(linetypes = [:path :steppre :steppost :sticks :scatter]),
+                :(n = length(linetypes)),
                 :(x = Vector[sort(rand(20)) for i in 1:n]),
                 :(y = rand(20,n)),
-                :(plot(x, y, line=(types,3), lab=map(string,types), ms=15))
+                :(plot(x, y, line=(linetypes,3), lab=map(string,linetypes), ms=15))
               ]),
   PlotExample("Line styles",
               "",
